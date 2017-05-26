ca1823461e777a958b3ad619e85abc33f5ff0f31
Charles1104 <charles.xavier.verleyen@gmail.com>
2017-05-25 15:44:55 -1000

    improvements done

--- models/Req_Prod_Requested.js
+++ models/Req_Prod_Requested.js
@@ -1,5 +1,10 @@
 module.exports = function(sequelize, DataTypes) {
   var Req_Prod_Requested = sequelize.define("Req_Prod_Requested", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     quantity:{
       type: DataTypes.INTEGER,
       allowNull: false
