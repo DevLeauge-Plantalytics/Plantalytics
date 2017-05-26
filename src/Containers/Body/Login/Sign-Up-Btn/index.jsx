0e9bf6d3b6535a6dfa45361ccc9d383bf7b9d115
Charles1104 <charles.xavier.verleyen@gmail.com>
2017-05-25 17:08:24 -1000

    redirect works on the login page

--- src/Containers/Body/Login/Sign-Up-Btn/index.jsx
+++ src/Containers/Body/Login/Sign-Up-Btn/index.jsx
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import {
+  Link
+} from 'react-router-dom';
 
 class SignUpButton extends Component {
   constructor(props) {
@@ -7,10 +10,9 @@ class SignUpButton extends Component {
   render(){
     return (
       <div id="sign-up-from-login">
-        <p id="sufl-Q">Don't have an account?</p>
-        <button id="sufl-btn">Sign Up</button>
+        <p id="sufl-Q">Dont have an account?</p>
+        <Link to="/plantalytics/sign-up"> <button id="sufl-btn">Sign Up</button></Link>
       </div>
-
     )
   }
 }
