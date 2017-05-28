d455676f60ec6c67397005a6b14100244f5443ea
Charles1104 <charles.xavier.verleyen@gmail.com>
2017-05-28 11:21:41 -1000

    D3 files ready for upload of data in seeder

--- models/D3rainfall.js
+++ models/D3rainfall.js
@@ -10,6 +10,14 @@ module.exports = function(sequelize, DataTypes) {
         type: DataTypes.INTEGER,
         allowNull: false,
       },
+      latitude:{
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+      longitude:{
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
     }
   );
   return D3rainfall;
