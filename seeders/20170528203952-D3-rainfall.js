005a20dc0e340379ed8f831dfb02ed00f7067b93
JSlovak <jasmine.slovak@gmail.com>
2017-05-29 11:24:39 -1000

    rainfall seed-data for 3 locations

--- seeders/20170528203952-D3-rainfall.js
+++ seeders/20170528203952-D3-rainfall.js
@@ -4,29 +4,292 @@ module.exports = {
   up: function (queryInterface, Sequelize) {
       return queryInterface.bulkInsert('D3rainfalls', [{
         month: 1,
-        rainfall: 100 ,
-        latitude: 41.40338,
-        longitude: 2.17403,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        month: 1,
-        rainfall: 100 ,
-        latitude: 41.40338,
-        longitude: 2.17403,
+        rainfall: 6.8,
+        latitude: 21.309052,
+        longitude: 157.711454,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         month: 2,
-        rainfall: 100 ,
-        latitude: 41.40338,
-        longitude: 2.17403,
+        rainfall: 4.6,
+        latitude: 21.309052,
+        longitude: 157.711454,
         createdAt: new Date(),
         updatedAt: new Date()
-      }
-      ], {});
+      },
+      {
+        month: 3,
+        rainfall: 3.6,
+        latitude: 21.309052,
+        longitude: 157.711454,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 4,
+        rainfall: 3.2,
+        latitude: 21.309052,
+        longitude: 157.711454,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 5,
+        rainfall: 3.2,
+        latitude: 21.309052,
+        longitude: 157.711454,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 6,
+        rainfall: 1.5,
+        latitude: 21.309052,
+        longitude: 157.711454,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 7,
+        rainfall: 1.6,
+        latitude: 21.309052,
+        longitude: 157.711454,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 8,
+        rainfall: 1.5,
+        latitude: 21.309052,
+        longitude: 157.711454,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 9,
+        rainfall: 2.0,
+        latitude: 21.309052,
+        longitude: 157.711454,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 10,
+        rainfall: 3.7,
+        latitude: 21.309052,
+        longitude: 157.711454,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 11,
+        rainfall: 5.6,
+        latitude: 21.309052,
+        longitude: 157.711454,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 12,
+        rainfall: 5.5,
+        latitude: 21.309052,
+        longitude: 157.711454,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 1,
+        rainfall: 8.5,
+        latitude: 21.471278,
+        longitude: -157.855609,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 2,
+        rainfall: 6.0,
+        latitude: 21.471278,
+        longitude: -157.855609,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 3,
+        rainfall: 6.5,
+        latitude: 21.471278,
+        longitude: -157.855609,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 4,
+        rainfall: 6.6,
+        latitude: 21.471278,
+        longitude: -157.855609,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 5,
+        rainfall: 5.0,
+        latitude: 21.471278,
+        longitude: -157.855609,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 6,
+        rainfall: 4.6,
+        latitude: 21.471278,
+        longitude: -157.855609,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 7,
+        rainfall: 5.1,
+        latitude: 21.471278,
+        longitude: -157.855609,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 8,
+        rainfall: 4.5,
+        latitude: 21.471278,
+        longitude: -157.855609,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 9,
+        rainfall: 4.9,
+        latitude: 21.471278,
+        longitude: -157.855609,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 10,
+        rainfall: 6.4,
+        latitude: 21.471278,
+        longitude: -157.855609,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 11,
+        rainfall: 9.5,
+        latitude: 21.471278,
+        longitude: -157.855609,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 12,
+        rainfall: 7.3,
+        latitude: 21.471278,
+        longitude: -157.855609,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 1,
+        rainfall: 4.1,
+        latitude: 21.467417,
+        longitude: 158.201618,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 2,
+        rainfall: 2.9,
+        latitude: 21.467417,
+        longitude: 158.201618,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 3,
+        rainfall: 2.4,
+        latitude: 21.467417,
+        longitude: 158.201618,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 4,
+        rainfall: 2.2,
+        latitude: 21.467417,
+        longitude: 158.201618,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 5,
+        rainfall: 1.7,
+        latitude: 21.467417,
+        longitude: 158.201618,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 6,
+        rainfall: 1.3,
+        latitude: 21.467417,
+        longitude: 158.201618,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 7,
+        rainfall: 1.3,
+        latitude: 21.467417,
+        longitude: 158.201618,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 8,
+        rainfall: 1.1,
+        latitude: 21.467417,
+        longitude: 158.201618,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 9,
+        rainfall: 1.3,
+        latitude: 21.467417,
+        longitude: 158.201618,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 10,
+        rainfall: 3.4,
+        latitude: 21.467417,
+        longitude: 158.201618,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 11,
+        rainfall: 3.4,
+        latitude: 21.467417,
+        longitude: 158.201618,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        month: 12,
+        rainfall: 4.3,
+        latitude: 21.467417,
+        longitude: 158.201618,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }], {});
   },
 
   down: function (queryInterface, Sequelize) {
