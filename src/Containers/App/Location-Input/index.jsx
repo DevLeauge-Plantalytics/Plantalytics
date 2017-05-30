8700d3bfc83bed665e9b01b249edb99c50614064
Charles1104 <charles.xavier.verleyen@gmail.com>
2017-05-29 15:14:50 -1000

    close #112

--- src/Containers/App/Location-Input/index.jsx
+++ src/Containers/App/Location-Input/index.jsx
@@ -8,7 +8,7 @@ class LocInput extends Component {
     return (
       <div id="loc-input">
         <h1 id="all-good-things">ALL GOOD THINGS GROWN, START AT HOME.</h1>
-        <input id="location-input" type="text" placeholder="Enter the address where you'll plant change"/>
+        <input id="location-input" type="text" placeholder="Enter the zip code where you'll plant change"/>
         <button id="get-started-btn">Search</button>
       </div>
     )
