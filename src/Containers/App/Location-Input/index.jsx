802cd6575166f4c0d5f9c05577c18829bf97f750
Charles1104 <charles.xavier.verleyen@gmail.com>
2017-05-29 20:03:37 -1000

    working on address input from homepage

--- src/Containers/App/Location-Input/index.jsx
+++ src/Containers/App/Location-Input/index.jsx
@@ -1,16 +1,54 @@
 import React, {Component} from 'react';
+
 class LocInput extends Component {
   constructor(props) {
     super(props);
-    this.title = 'No-Warning'
+    this.state = {
+      address: "",
+    };
   }
+
+  latlong(address){
+    return fetch(`/localisation/${address}`, {
+        credentials: 'include',
+      })
+      .then( res => res.json())
+      .catch(err => {
+        console.log(err);
+        throw err;
+      })
+      .then( location => {
+        user.latitude = location[0].latitude,
+        user.longitude = location[0].longitude,
+        this.props.addUser(user);
+      })
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.addUser(this.state);
+  }
+
+  handleChange = (event) => {
+    event.preventDefault();
+    this.setState({[event.target.name]: event.target.value})
+  }
+
   render(){
     return (
-      <div id="loc-input">
+      <form id="loc-input">
         <h1 id="all-good-things">ALL GOOD THINGS GROWN, START AT HOME.</h1>
-        <input id="location-input" type="text" placeholder="Enter the zip code where you'll plant change"/>
-        <button id="get-started-btn">Search</button>
-      </div>
+        <input
+          id="location-input"
+          type="text"
+          placeholder="Enter the address where you'll plant change"
+          value={this.state.address}
+          onChange={this.handleChange}
+          name="email"
+        required/>
+        <button type="submit">Search</button>
+      </form>
     )
   }
 }
