2f8da6fd45a7753c809e237477697c143cc8f395
Charles1104 <charles.xavier.verleyen@gmail.com>
2017-05-31 13:31:37 -1000

    close #135 - information displayed according to logged in or not

--- front-end/src/Components/Header/index.jsx
+++ front-end/src/Components/Header/index.jsx
@@ -2,36 +2,58 @@ import React, {Component} from 'react';
 import Navigation from './Navigation';
 import ProfilePic from './Profile-Pic';
 import './styles.css';
-import {connect} from 'react-redux'
+import {Link} from 'react-router-dom';
+import {signOut} from '../../Actions';
+import {connect} from 'react-redux';
+import {withRouter} from 'react-router';
+
 class Header extends Component {
   constructor(props) {
     super(props);
-    this.title = 'No-Warning'
+    this.title = 'No-Warning';
+    this.logout = this.logout.bind(this);
   }
+
+  logout(event) {
+    this.props.signOut();
+    this.props.history.push('/home')
+  }
+
   render(){
     return (
       <div id="header">
-      {this.props.loggedIn &&
-          <p> You are logged in as {localStorage.username}</p>
+        {this.props.loggedIn &&
+          <div>
+            <p id="loggedInfo"> You are logged in as {localStorage.username}</p>
+            <p onClick={this.logout} className="home-header-item">&nbsp;&nbsp;&nbsp;Logout</p>
+          </div>
         }
         {!this.props.loggedIn &&
-          <p> You are currently not logged in</p>
+          <div>
+            <p id="loggedInfo"> You are currently not logged in</p>
+            <Link className="home-header-link" to="/login"><p className="home-header-item">Login</p></Link>
+            <p className="home-header-slash">&nbsp;/&nbsp;</p>
+            <Link className="home-header-link" to="/sign-up"><p className="home-header-item">Sign-Up</p></Link>
+          </div>
         }
         <Navigation/>
-        <ProfilePic/>
+        {this.props.loggedIn &&
+          <ProfilePic/>
+        }
       </div>
     )
   }
 }
-
 const mapStateToProps = state => {
   return {
     loggedIn: state.users.loggedIn
   }
 }
-
 const mapDispatchToProps = dispatch => {
   return {
+    signOut: () => {
+      dispatch(signOut())
+    }
   }
 }
-export default connect(mapStateToProps, mapDispatchToProps)(Header)
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Header))
